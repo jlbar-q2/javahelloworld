4cd40912d947789c023a6df00b2f08a15edae0e1
Jesse Barbour <jbarbour@q2ebanking.com>
2015-07-06 18:13:31 -0500

    first commit for docker helloworld CI test

---
+++ JavaHelloWorld.java
@@ -0,0 +1,13 @@
+/*
+Just a Hello World for Docker testing
+*/
+
+public class JavaHelloWorld
+{
+	public static void main (String [] args)
+	{
+		System.out.println("Java Hello World");
+		System.out.println("Hello AGAIN!");
+	}
+}
+
